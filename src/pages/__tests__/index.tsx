fca8d55b61271593534f1260999245418ac9a6e6
renangarciasoz <renangarciasoz@hotmail.com>
2022-04-03 16:03:00 -0300

    remove __tests__ from page folder

--- src/pages/__tests__/index.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "utils/test-utils";
-import HomePage from "../index";
-
-describe("Home page", () => {
-  it("renders correctly", () => {
-    const { asFragment } = render(<HomePage />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
